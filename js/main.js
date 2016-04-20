8f1b7e8a96e55a47a3c9c1e3331f1c598fc3e093
Joseph Walker <joseph.walker@bbc.co.uk>
2016-04-20 20:38:00 +0100

    start gallery

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
-    }
-  });
-});
-
-
 $(function() {   
     $('.nav__button').on('click', function(e) {
-      $('.nav__list').toggleClass("nav--active");
-      e.preventDefault();
-    });
-});
-
-
-$(function() {   
-    $('.nav__item').on('click', function(e) {
       $('.nav__list').toggleClass("nav--active");
       e.preventDefault();
     });
